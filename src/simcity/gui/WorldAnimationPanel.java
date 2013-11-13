5a987d12746c2509756258f74067bec59ff29119
mich326 <mich326@usc.edu>
2013-11-12 20:28:22 -0800

    Adding skeleton WorldAnimationPanel.java file

---
+++ src/simcity/gui/WorldAnimationPanel.java
@@ -0,0 +1,19 @@
+package simcity.gui;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class WorldAnimationPanel extends JPanel implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}

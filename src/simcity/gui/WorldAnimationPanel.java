287e5ba88b2b409bc2ba98d6eaf75a198087ac05
mich326 <mich326@usc.edu>
2013-11-12 20:41:54 -0800

    Adding Basic Gui.java file

--- src/simcity/gui/WorldAnimationPanel.java
+++ src/simcity/gui/WorldAnimationPanel.java
@@ -5,14 +5,23 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
 
-public class WorldAnimationPanel extends JPanel implements ActionListener {
+public class WorldAnimationPanel extends AnimationPanel implements ActionListener {
 
+	
+    private List<Gui> guis = Collections.synchronizedList(new ArrayList<Gui>());
+
+	
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	
 
 	
 	

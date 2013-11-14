16d77e550cbfae669f32d0d96c778e328a26aec8
clay-to-n <cbrand@usc.edu>
2013-11-13 23:55:44 -0800

    Changed variable names in ControlPanel to be a bit more clear as to the tabs

--- src/simcity/gui/ControlPanel.java
+++ src/simcity/gui/ControlPanel.java
@@ -37,53 +37,53 @@ public class ControlPanel extends JPanel implements ActionListener {
 	private JTabbedPane tabPane = new JTabbedPane();
 	private JPanel bottomSpace = new JPanel();
 	
-	//tab1 (Settings) elements
-	private JPanel tab1 = new JPanel();
+	//settingsTab (Settings) elements
+	private JPanel settingsTab = new JPanel();
 	private JSlider globalSpeed = new JSlider();
 	private JLabel globalSpeedLabel = new JLabel("Global Speed");
 
-	//tab2 (Log) world console elements
-	private JPanel tab2 = new JPanel();
+	//logTab (Log) world console elements
+	private JPanel logTab = new JPanel();
 	private JLabel worldConsoleLabel = new JLabel("<html><u><b>World Console</b></u></html>");
 	private JLabel zoomConsoleLabel = new JLabel("<html><u><b>Zoom Console</b></u></html>");
 	private JPanel topPanel = new JPanel();
-		//tab2: errors elements
+		//logTab: errors elements
 	private JLabel errorsLabel = new JLabel("<html><b>Errors:</b></html>");
 	private JRadioButton outsideErrors = new JRadioButton("Outside");
 	private JRadioButton allErrors = new JRadioButton("All");
 	private JRadioButton noErrors = new JRadioButton("None");
 	private JPanel errorButtonsPanel = new JPanel();
-		//tab2: communication elements
+		//logTab: communication elements
 	private JPanel commPanel = new JPanel();
 	private JLabel commLabel = new JLabel("<html><b>Communication:</b></html>");
 	private JCheckBox pedestrian = new JCheckBox("Pedestrians");
 	private JCheckBox busPass = new JCheckBox("Bus Passengers");
 	private JCheckBox carPass = new JCheckBox("Car Passengers");
-		//tab2: print role switches or not
+		//logTab: print role switches or not
 	private JPanel roleSwitchPanel = new JPanel();
 	private JLabel roleSwitch = new JLabel("<html><b>Role Switches:</b></html>");
 	private JCheckBox roleSwitchCB = new JCheckBox();
-	//tab2 (Log) zoom console
+	//logTab (Log) zoom console
 	private JPanel zoomPanel = new JPanel();
 	private JPanel zoomErrorsPanel = new JPanel();
 	private JLabel errors2Label = new JLabel("<html><b>Errors:</b></html>");
 	private JCheckBox errorsCB = new JCheckBox();
-		//tab2: communication zoom elements
+		//logTab: communication zoom elements
 	private JPanel comm2Panel = new JPanel();
 	private JLabel comm2Label = new JLabel("<html><b>Communication:</b></html>");
 	private JCheckBox commCB = new JCheckBox();
-		//tab2: role switching zoom elements
+		//logTab: role switching zoom elements
 	private JPanel roleSwitch2Panel = new JPanel();
 	private JLabel roleSwitch2 = new JLabel("<html><b>Role Switches:</b></html>");
 	private JCheckBox roleSwitch2CB = new JCheckBox();
 	
-	//tab3 - controls the building or person selected
-	private JPanel tab3 = new JPanel();
+	//selectTab - controls the building or person selected
+	private JPanel selectTab = new JPanel();
 	private JLabel nameLabel = new JLabel("Name: Ralph's");
 	private JLabel typeLabel = new JLabel("Type: Market");
 	
-	//tab4 - global events
-	private JPanel tab4 = new JPanel();
+	//eventsTab - global events
+	private JPanel eventsTab = new JPanel();
 	private JButton earthquake = new JButton("Earthquake");
 	private JButton fire = new JButton("Fire (In Selected Building)");
 	
@@ -117,14 +117,14 @@ public class ControlPanel extends JPanel implements ActionListener {
 		
 		tabPane.setPreferredSize(new Dimension(300, 650));
 		
-		//tab1 (Settings)
-		tab1.setLayout(new FlowLayout());
+		//settingsTab (Settings)
+		settingsTab.setLayout(new FlowLayout());
 		globalSpeedLabel.setAlignmentX(CENTER_ALIGNMENT);
-		tab1.add(globalSpeedLabel);
-		tab1.add(globalSpeed);
+		settingsTab.add(globalSpeedLabel);
+		settingsTab.add(globalSpeed);
 		
 		//Log tab world section
-		tab2.setLayout(new FlowLayout());
+		logTab.setLayout(new FlowLayout());
 		topPanel.setLayout(new GridLayout(3, 1));
 		worldConsoleLabel.setAlignmentX(CENTER_ALIGNMENT);
 		worldConsoleLabel.setFont(new Font("Arial", Font.BOLD, 16));
@@ -132,7 +132,7 @@ public class ControlPanel extends JPanel implements ActionListener {
 		topPanel.add(worldConsoleLabel);
 		topPanel.add(new JLabel());
 		topPanel.add(errorsLabel);
-		tab2.add(topPanel);
+		logTab.add(topPanel);
 		
 		//log tab world errors section
 		allErrors.setSelected(false);
@@ -144,7 +144,7 @@ public class ControlPanel extends JPanel implements ActionListener {
 		errorButtonsPanel.add(outsideErrors);
 		errorButtonsPanel.add(allErrors);
 		errorButtonsPanel.add(noErrors);
-		tab2.add(errorButtonsPanel);
+		logTab.add(errorButtonsPanel);
 		
 		//log tab world communications section
 		commPanel.setLayout(new GridLayout(4, 1));
@@ -158,14 +158,14 @@ public class ControlPanel extends JPanel implements ActionListener {
 		commPanel.add(pedestrian);
 		commPanel.add(busPass);
 		commPanel.add(carPass);
-		tab2.add(commPanel);
+		logTab.add(commPanel);
 		
 		//log tab role switching
 		roleSwitchCB.setSelected(true);
 		roleSwitchCB.addActionListener(this);
 		roleSwitchPanel.add(roleSwitch);
 		roleSwitchPanel.add(roleSwitchCB);
-		tab2.add(roleSwitchPanel);
+		logTab.add(roleSwitchPanel);
 		
 		//log tab zoom section
 		zoomPanel.setLayout(new GridLayout(5, 1));
@@ -192,26 +192,26 @@ public class ControlPanel extends JPanel implements ActionListener {
 		roleSwitch2Panel.add(roleSwitch2);
 		roleSwitch2Panel.add(roleSwitch2CB);
 		zoomPanel.add(roleSwitch2Panel);
-		tab2.add(zoomPanel);
+		logTab.add(zoomPanel);
 		
-		//tab3
-		tab3.setLayout(new GridLayout(15, 1));
-		tab3.add(nameLabel);
-		tab3.add(typeLabel);
+		//selectTab
+		selectTab.setLayout(new GridLayout(15, 1));
+		selectTab.add(nameLabel);
+		selectTab.add(typeLabel);
 		
-		//tab4
-		tab4.setLayout(new GridLayout(15, 1));
+		//eventsTab
+		eventsTab.setLayout(new GridLayout(15, 1));
 		earthquake.addActionListener(this);
 		fire.addActionListener(this);
-		tab4.add(earthquake);
-		tab4.add(fire);
+		eventsTab.add(earthquake);
+		eventsTab.add(fire);
 		
 		
 		
-		tabPane.addTab("Control", tab3);
-		tabPane.addTab("Settings", tab1);
-		tabPane.addTab("Log", tab2);
-		tabPane.addTab("Events", tab4);
+		tabPane.addTab("Selected", selectTab);
+		tabPane.addTab("Settings", settingsTab);
+		tabPane.addTab("Log", logTab);
+		tabPane.addTab("Events", eventsTab);
 		
 		
 		

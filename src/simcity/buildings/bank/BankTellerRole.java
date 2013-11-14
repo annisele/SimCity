ab037a7b3298c5b3672c58dfefeed3395a01b720
Kevin Chan <kevinalc@usc.edu>
2013-11-14 13:26:44 -0800

    Added skeleton and messages to BankTellerRole

--- src/simcity/buildings/bank/BankTellerRole.java
+++ src/simcity/buildings/bank/BankTellerRole.java
@@ -1,5 +1,98 @@
 package simcity.buildings.bank;
 
-public class BankTellerRole implements simcity.interfaces.Person {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import simcity.interfaces.bank.BankCustomer;
+
+public class BankTellerRole implements simcity.interfaces.bank.BankTeller {
+
+	// data
+	private List<MyCustomer> customers = Collections.synchronizedList(new ArrayList<MyCustomer>());		// list of customers
+	BankSystem bank;	// bank system that contains account info for people
+
+	public enum transactionType {none, openAccount, depositMoney, withdrawMoney, loanMoney};
+	public enum transactionState {none, processing};
+
+	// constructor
+
+	// messages
+
+	public void msgWantToOpenAccount(BankCustomer bc, double amountToProcess) {
+    	customers.add(new MyCustomer(bc, amountToProcess, transactionType.openAccount));
+    	//stateChanged();
+	}
+
+	public void msgWantToDeposit(BankCustomer bc, double amountToProcess) {
+    	customers.add(new MyCustomer(bc, amountToProcess, transactionType.depositMoney));
+    	// stateChanged();
+	}
+
+	public void msgWantToWithdraw(BankCustomer bc, double amountToProcess) {
+    	customers.add(new MyCustomer(bc, amountToProcess, transactionType.withdrawMoney));
+    	// stateChanged();
+	}
+
+	public void msgWantALoan(BankCustomer bc, double amountToProcess) {
+    	customers.add(new MyCustomer(bc, amountToProcess, transactionType.loanMoney));
+    	// stateChanged();
+	}
+
+	// scheduler
+
+	// actions
+
+	// animation DoXYZ
+
+	// utility classes
+	public class MyCustomer {
+		BankCustomer bc;
+		double amountToProcess;
+		transactionType tt;
+		transactionState ts;
+
+		MyCustomer(BankCustomer bc, double amountToProcess, transactionType tt) {
+			this.bc = bc;
+			this.amountToProcess = amountToProcess;
+			this.tt = tt;
+			this.ts = transactionState.none;
+		}
+
+		public BankCustomer getBankCustomer() {
+			return bc;
+		}
+
+		public void setBankCustomer(BankCustomer bc) {
+			this.bc = bc;
+		}
+
+		public double getAmountToProcess() {
+			return amountToProcess;
+		}
+
+		public void setAmountToProcess(double amountToProcess) {
+			this.amountToProcess = amountToProcess;
+		}
+
+		public transactionType getTransactionType() {
+			return tt;
+		}
+
+		public void setTransactionType(transactionType tt) {
+			this.tt = tt;
+		}
+
+		public transactionState getTransactionState() {
+			return ts;
+		}
+
+		public void setTransactionState(transactionState ts) {
+			this.ts = ts;
+		}
+
+	}
+
+	// utility functions
 
 }

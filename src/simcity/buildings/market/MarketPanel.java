cb66a2f61a26d100ae66c2da5a4a0ac8a77091e1
rhao <rhao@usc.edu>
2013-11-14 00:28:21 -0800

    Added market setup files such as market panel

---
+++ src/simcity/buildings/market/MarketPanel.java
@@ -0,0 +1,49 @@
+package simcity.buildings.market;
+
+import java.util.*;
+import javax.swing.JPanel;
+
+import simcity.gui.SimCityGui;
+import simcity.gui.market.MarketAnimationPanel;
+import simcity.gui.market.MarketControlPanel;
+import simcity.interfaces.market.MarketCashier;
+import simcity.interfaces.market.MarketCustomer;
+import simcity.interfaces.market.MarketTruck;
+import simcity.interfaces.market.MarketWorker;
+
+/*******************
+ * Market Panel - Controls everything within the panel. Creates the roles and GUI's.
+ * @author rebeccahao
+ *
+ */
+public class MarketPanel extends JPanel {
+
+	private SimCityGui simCityGui;
+	public MarketControlPanel controlPanel;
+	public MarketAnimationPanel animationPanel;
+	private MarketSystem system = new MarketSystem();;
+	private MarketCashier cashier;
+	private List<MarketCustomer> customers = Collections.synchronizedList(new ArrayList<MarketCustomer>());
+	private List<MarketTruck> trucks = Collections.synchronizedList(new ArrayList<MarketTruck>());
+	private List<MarketWorker> workers = Collections.synchronizedList(new ArrayList<MarketWorker>());
+
+	public MarketPanel(SimCityGui scg) {
+		super();
+		controlPanel = new MarketControlPanel();
+		animationPanel = new MarketAnimationPanel(scg);
+	}
+	
+	//replaces existing inventory with passed in one
+	public void setInventory(Map<String, Integer> inv) {
+		system.setInventory(inv);
+	}
+	
+	//sets the cashier
+	public void setCashier(MarketCashier c) {
+		cashier = c;
+	}
+	
+	
+	
+	
+}
--- src/simcity/gui/market/MarketAnimationPanel.java
+++ src/simcity/gui/market/MarketAnimationPanel.java
@@ -5,23 +5,17 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.*;
 
-import simcity.buildings.market.*;
 import simcity.gui.AnimationPanel;
 import simcity.gui.SimCityGui;
-import simcity.interfaces.market.*;
 
 
-//TEST - this is just a test to put into zoom panel
-
+/****************
+ * Market Animation Panel - Controls market GUI.
+ * @author rebeccahao
+ *
+ */
 public class MarketAnimationPanel extends AnimationPanel implements ActionListener  {
-
-	private MarketSystem system;
-	private MarketCashier cashier;
-	private List<MarketCustomer> customers = Collections.synchronizedList(new ArrayList<MarketCustomer>());
-	private List<MarketTruck> trucks = Collections.synchronizedList(new ArrayList<MarketTruck>());
-	private List<MarketWorker> workers = Collections.synchronizedList(new ArrayList<MarketWorker>());
 
 	
 	public MarketAnimationPanel(SimCityGui sc) {
@@ -35,15 +29,11 @@ public class MarketAnimationPanel extends AnimationPanel implements ActionListen
 	
 	public void paintComponent(Graphics g) {
 		Graphics2D g2 = (Graphics2D)g;
-        //Clear the screen by painting a rectangle the size of the frame
-		
+       
+		//Clear the screen by painting a rectangle the size of the frame
 		g2.setColor(Color.LIGHT_GRAY);
 	    g2.fillRect(0, 0, this.getWidth(), this.getHeight());
-	    g2.setColor(Color.DARK_GRAY);
-	    for(int i = 0; i < 8; i++) {
-	    	g2.fillRect(50 + (50*i), 100, 20, 200);
-	    }
-	    
+	  
 		super.paintComponent(g);
 		
 		//super.guis.add(bob);

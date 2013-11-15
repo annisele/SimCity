3e330299d7cf5ad098c211e596f4a90eede87cb4
levonne key <levonnekey@guest-wireless-upc-nat-206-117-88-071.usc.edu>
2013-11-14 17:09:04 -0800

    updated bank

--- src/simcity/buildings/bank/BankCustomerRole.java
+++ src/simcity/buildings/bank/BankCustomerRole.java
@@ -1,5 +1,145 @@
 package simcity.buildings.bank;
 
+import java.util.*;
 public class BankCustomerRole implements simcity.interfaces.bank.BankCustomer {
+	private BankHostRole bh;
+	private BankTellerRole bt;
+	private String name;
+	Timer timer = new Timer();
+	public enum AgentState{DoingNothing, WaitingInBank, AtWindow, TellerProcessingTransaction, RequestLoan};
+	private AgentState state = AgentState.DoingNothing;//The start state
+	public enum AgentEvent{haveBankTransaction, beingHelpedAtWindow, getAccountInfo, withdrawMoney, depositMoney, requestLoan, requestOpenAccount, loanApproved, loanUnapproved, doneTransaction};
+    AgentEvent event;   
+	//hack to establish connection to BankHostRole 
+	public void setBankHost(BankHostRole bh) {
+		this.bh = bh;
+	}
+	//hack to establish connection to BankTellerRole 
+	public void setBankTeller(BankTellerRole bt) {
+		this.bt = bt;
+	}
+	public String getCustomerName() {
+		return name;
+	}     
+	//messages 
+	
+	//bank host sends this message to tell bank customer to go to bank window
+	public void msgGoToWindow(int windowNumber, BankTellerRole bt) {
+		this.bt = bt;
+		event = AgentEvent.beingHelpedAtWindow;
+		System.out.println("Please go to that window");
+		//stateChanged();
+	}
+	// bank teller sends this message to customer after opening an account
+	public void msgHereIsAccountInfo(int accountNumber, double accountBalance) {
+		System.out.println("Here is your new account information");
+		event = AgentEvent.getAccountInfo;
+		//stateChanged();
+	}
+	//bank teller sends this message to customer after withdrawing money
+	public void msgHerIsMoney(int accountNumber, double accountBalance, double withdrawAmount) {
+		System.out.println("Here is the money that you withdraw");
+		event = AgentEvent.withdrawMoney;
+		//stateChanged(); 
+	}
+	//bank teller sends this message to customer after depositing money
+	public void msgMoneyIsDeposited(int accountNumber, double accountBalance, double depositAmount) {
+		System.out.println("Here is the money that you withdraw");
+		event = AgentEvent.depositMoney;
+		//stateChanged();
+    }
+	//bank teller sends this message to customer when the loan is approved
+	 public void msgHereIsYourLoan(int accountNumber, double accountBalance, double loanAmount) {
+		    System.out.println("Your loan has been approved");
+		    event = AgentEvent.loanApproved;
+			//stateChanged(); 
+	 }
+	//bank teller sends this message to customer when the loan is not approved
+	 public void msgCannotGrantLoan(int accountNumber, double accountBalance, double loanAmount) {
+		 System.out.println("Your loan is not approved");
+    	 event = AgentEvent.loanUnapproved;
+	     //stateChanged(); 
+     }
+	 //bank teller sends this message to customer when the transaction is completed
+	 public void msgDoneTransaction() {
+		    System.out.println("Your transaction is completed");
+			event = AgentEvent.doneTransaction;
+			//stateChanged(); 
+	 }
+	 //scheduler
+	 protected boolean pickAndExecuteAnAction() {
+		 if (state == AgentState.DoingNothing){
+			    if (event == AgentEvent.haveBankTransaction){
+				goingToBank();
+				state = AgentState.WaitingInBank;
+				return true;
+			    }
+			    
+			}
+			if (state == AgentState.WaitingInBank) {
+			    if (event == AgentEvent.beingHelpedAtWindow) {
+				makeTransactionChoice();
+				state = AgentState.AtWindow;
+				return true;
+			    }
+			}
+			if (state == AgentState.AtWindow) {
+			   
+				if (event == AgentEvent.withdrawMoney)	{
+					withdrawMoney();
+					state = AgentState.TellerProcessingTransaction;
+				return true;
+			    }
+				}
+			if (state == AgentState.AtWindow) {
+				if (event == AgentEvent.depositMoney) {
+					depositMoney();	
+					state = AgentState.TellerProcessingTransaction;
+				return true;
+			    }
+			}
+			if (state == AgentState.AtWindow) {
+				if (event == AgentEvent.requestOpenAccount){
+					openAccount();
+					state = AgentState.TellerProcessingTransaction;
+				return true;
+			    }
+			}
+			if (state == AgentState.AtWindow) {
+				if (event == AgentEvent.requestLoan)	{
+				requestLoan();
+				state = AgentState.TellerProcessingTransaction;
+				return true;
+			    }
+			}
+				
+			if (state == AgentState.RequestLoan) {
+			    if (event == AgentEvent.loanApproved)	{
+				obtainLoan();
+				}
+				else if (event == AgentEvent.loanUnapproved) {
+				doesNotObtainLoan();
+				}	
+				state = AgentState.TellerProcessingTransaction;
+				return true;
+			    
+			}
+
+			if (state == AgentState.TellerProcessingTransaction) {
+			    if (event == AgentEvent.doneTransaction)	{
+				leaveBank();
+				state = AgentState.DoingNothing;
+				return true;
+			    }
+			}
+			System.out.println("No scheduler rule fired, should not happen in FSM, event="+event+" state="+state);
+
+		 return false;
+	 }
+	 //actions
+	 private void goingToBank() {
+			//DoGoingToBank();//animation
+			bh.msgEnteringBank(this);
+	 }    
 
 }
--- src/simcity/buildings/bank/BankHostRole.java
+++ src/simcity/buildings/bank/BankHostRole.java
@@ -1,19 +1,35 @@
 package simcity.buildings.bank;
 
-import java.util.Collections;
 import java.util.*;
 public class BankHostRole implements simcity.interfaces.bank.BankHost {
 	private class Window {
+		public BankCustomerRole occupiedBy;
 		public int windowNum;
-		private boolean occupied = false;
+		public boolean occupied = false;
 		public boolean isOccupied() {
 			return occupied;
 		}
 		public int getWindowNumber() {
 			return windowNum;
 		}
+		public void setUnoccupied() {
+			occupiedBy = null;
+		}
+		public void setOccupant(BankCustomerRole cust) {
+			occupiedBy = cust;
+		}
+		BankCustomerRole getOccupant() {
+			return occupiedBy;
+		}
+		public Window(int windowNum) {
+			this.windowNum = windowNum;
+			this.occupied = false;
+		}
 	}
 	String name;
+	public String getName() {
+		return name;
+	}
 	Timer timer = new Timer();
 	public List<Window> windows = Collections.synchronizedList(new ArrayList<Window>());
 	public List<BankCustomerRole> bc = Collections.synchronizedList(new ArrayList<BankCustomerRole>());
@@ -25,10 +41,16 @@ public class BankHostRole implements simcity.interfaces.bank.BankHost {
 
 	//messages
 	public void msgEnteringBank(BankCustomerRole bc) {
-		
+		System.out.println("Bank customer is entering the bank");
 	}
 	public void msgLeavingBank(int windowNumber) {
-		
+		System.out.println("Bank customer is leaving the bank");
+		for (Window window : windows) {
+			if (windowNumber == window.getWindowNumber()) {
+				window.setUnoccupied();
+				
+			}
+		}
 	}
 	//scheduler
 	protected boolean pickAndExecuteAnAction() {
@@ -44,6 +66,7 @@ public class BankHostRole implements simcity.interfaces.bank.BankHost {
 	}
 	//actions
 	private void tellCustomerToGoToWindow(BankCustomerRole bc, Window window) {
+		
 		
 	}
 }

14a9a0ae195daa4a59edae37022940a736081ce2
clay-to-n <cbrand@usc.edu>
2013-11-15 15:52:23 -0800

    Changed permissions in the system class

--- src/simcity/gui/market/MarketControlPanel.java
+++ src/simcity/gui/market/MarketControlPanel.java
@@ -1,11 +1,13 @@
 package simcity.gui.market;
 
+import javax.swing.JPanel;
+
 /******************
  * Market Control Panel - When market is selected, this will show up under Select tab in the control panel.
  * @author rebeccahao
  *
  */
-public class MarketControlPanel {
+public class MarketControlPanel extends JPanel {
 	
 	public MarketControlPanel() {
 		

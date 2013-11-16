c70336194049e44212a6d45ae0d71ddd1827232d
clay-to-n <cbrand@usc.edu>
2013-11-15 16:50:31 -0800

    Changing System stuff still

--- src/simcity/buildings/restaurant/five/RestaurantFiveSystem.java
+++ src/simcity/buildings/restaurant/five/RestaurantFiveSystem.java
@@ -1,5 +1,14 @@
 package simcity.buildings.restaurant.five;
 
+import simcity.gui.SimCityGui;
+import simcity.gui.WorldAnimationPanel;
+
+
 public class RestaurantFiveSystem extends System {
+	
+	public RestaurantFiveSystem(SimCityGui sc) {
+		super(sc);
+		animationPanel = new RestaurantFiveAnimationPanel();
+	}
 
 }

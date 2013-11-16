0513105512a3ddac95797ccefc03ddc2bdbd171c
levonne key <levonnekey@guest-wireless-upc-nat-206-117-88-071.usc.edu>
2013-11-15 16:57:51 -0800

    updated bankanimationpanel

---
+++ src/simcity/gui/bank/BankControlPanel.java
@@ -0,0 +1,16 @@
+package simcity.gui.bank;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
+import simcity.gui.AnimationPanel;
+import simcity.gui.Gui;
+import simcity.gui.SimCityGui;
+
+/****************
+ * Bank Animation Panel - 
+ * @author levonne key
+ */

7926baca0b469b772c9d8d7338c47b36c342c176
mich326 <mich326@usc.edu>
2013-11-19 21:44:50 -0800

    Adding in WRestaurantOneWaiterRole with only a few errors- added some interface stuff too

--- src/simcity/buildings/restaurant/one/RestaurantOneCookRole.java
+++ src/simcity/buildings/restaurant/one/RestaurantOneCookRole.java
@@ -1,6 +1,13 @@
 package simcity.buildings.restaurant.one;
 
 public class RestaurantOneCookRole {
+
+	public void msgHereIsAnOrder(
+			RestaurantOneWaiterRole restaurantOneWaiterRole, String choice,
+			int tnumber) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 }
--- src/simcity/buildings/restaurant/one/RestaurantOneCustomerRole.java
+++ src/simcity/buildings/restaurant/one/RestaurantOneCustomerRole.java
@@ -23,6 +23,11 @@ public class RestaurantOneCustomerRole extends Role implements simcity.interface
 		// TODO Auto-generated method stub
 		return false;
 	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 	
 }

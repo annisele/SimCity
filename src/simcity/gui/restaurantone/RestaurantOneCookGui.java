b3068d1f906f44b04ad7dc6b09b2f8b99678c4c0
mich326 <mich326@usc.edu>
2013-11-21 01:38:24 -0800

    Added code for RestaurantOneCookRole- Errors because I'm not sure whether to use internal market or not. Untested.

---
+++ src/simcity/gui/restaurantone/RestaurantOneCookGui.java
@@ -0,0 +1,40 @@
+package simcity.gui.restaurantone;
+
+import java.awt.Graphics2D;
+import java.awt.Point;
+
+import simcity.gui.Gui;
+
+public class RestaurantOneCookGui implements Gui {
+
+	@Override
+	public void updatePosition() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isPresent() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean contains(Point point) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void putongrill() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}

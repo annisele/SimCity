8b494c383b018c735ec7211c08705714a2565dde
Kevin Chan <kevinalc@usc.edu>
2013-11-22 00:56:23 -0800

    Deleted Transaction.java and edited BankTransaction

--- src/simcity/buildings/bank/BankTransaction.java
+++ src/simcity/buildings/bank/BankTransaction.java
@@ -4,13 +4,20 @@ import java.util.*;
 
 public class BankTransaction {
 	
-	 enum transactionType {none, openAccount, depositMoney, withdrawMoney, loanMoney, payLon, payRent};
+	 enum transactionType {none, openAccount, depositMoney, withdrawMoney, loanMoney, payLoan, payRent};
+	 transactionType type = transactionType.none;
 	 int accountID;
 	 double amountProcessed;
 	 String pinCode;
 	 boolean transactionSuccess = true;
 	 int landlordAccountID;
 	
+	 public transactionType getTransactionType() {
+		 return type;
+	 }
+	 public void setTransactionType(transactionType type) {
+		 this.type = type;
+	 }
 	 public int getAccountID() {
 		 return accountID;
 	 }
@@ -20,7 +27,7 @@ public class BankTransaction {
 	 public double getAmountProcessed() {
 		 return amountProcessed;
 	 }
-	 public void setAmountProcessed(double amountProcesed) {
+	 public void setAmountProcessed(double amountProcessed) {
 		 this.amountProcessed = amountProcessed;
 	 }
 	 public String getPinCode() {
@@ -32,7 +39,7 @@ public class BankTransaction {
 	 public int getLandlordAccountID() {
 		 return landlordAccountID;
 	 }
-	 public void setLandlordAccountID(int landlordAccount) {
+	 public void setLandlordAccountID(int landlordAccountID) {
 		 this.landlordAccountID = landlordAccountID;
 	 }
 }

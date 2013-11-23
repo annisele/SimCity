6c0356b390a33823f993e5f0090d1007a49d2eb3
rhao <rhao@usc.edu>
2013-11-22 22:48:38 -0800

    Fixed animation so person can enter buildings and switch panels

--- src/simcity/gui/market/MarketAnimationPanel.java
+++ src/simcity/gui/market/MarketAnimationPanel.java
@@ -29,15 +29,13 @@ public class MarketAnimationPanel extends AnimationPanel implements ActionListen
 	    Image img = ii.getImage();
 	    Image marketimage = img.getScaledInstance(470, 454,  java.awt.Image.SCALE_SMOOTH); 
 	
-	private List<Gui> guis = new ArrayList<Gui>();
-
 	public MarketAnimationPanel() {
 		super();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-
+		super.actionPerformed(arg0);
 	}
 
 	public void paintComponent(Graphics g) {
@@ -46,23 +44,6 @@ public class MarketAnimationPanel extends AnimationPanel implements ActionListen
 		//Clear the screen by painting a rectangle the size of the frame
 		g2.setColor(Color.LIGHT_GRAY);
 		g2.fillRect(0, 0, this.getWidth(), this.getHeight());
-
-		//walls
-		g2.setColor(Color.GRAY);
-		g2.fillRect(100 , 0, this.getWidth(), WALL_WIDTH); //top wall
-		g2.fillRect(0 , 40, WALL_WIDTH, this.getHeight()); //left wall
-		g2.fillRect(this.getWidth() - WALL_WIDTH , 0, WALL_WIDTH, this.getHeight()); //right wall
-		g2.fillRect(0 , this.getHeight() - WALL_WIDTH, this.getWidth(), WALL_WIDTH); //bottom wall
-
-		//cash register
-		g2.setColor(Color.DARK_GRAY);
-		g2.fillRect(250, 50, 75, 30);
-		
-		//shelves
-		g2.setColor(new Color(100, 65, 45));
-		for(int i = 0; i < NUM_SHELVES; i++) {
-			g2.fillRect((i * 70) + 40, 150, 30, 250);
-		}
 		
 		g2.drawImage(marketimage, 0,0, null); //COMMENT this out to get old Gui Back- Josh
 
--- src/simcity/gui/market/MarketCustomerGui.java
+++ src/simcity/gui/market/MarketCustomerGui.java
@@ -5,13 +5,19 @@ import java.awt.Graphics2D;
 import java.awt.Point;
 
 import simcity.Location;
+import simcity.buildings.market.MarketCustomerRole;
 import simcity.gui.Gui;
 import simcity.interfaces.market.MarketCustomer;
 
 public class MarketCustomerGui extends Gui {
 	
-	private final int CASHIER_X = 300;
-	private final int CASHIER_Y = 100;
+	private final int CASHIER_X = 200;
+	private final int CASHIER_Y = 300;
+	
+	
+	public MarketCustomerGui(MarketCustomerRole m) {
+		role = m;
+	}
 	
 	@Override
 	public void draw(Graphics2D g) {

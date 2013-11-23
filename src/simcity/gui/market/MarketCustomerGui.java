72aeb894148d6d8a557f9e36cb2c6e1e3dd16d56
mich326 <mich326@usc.edu>
2013-11-23 00:00:57 -0800

    Added in a gui that resembles a person

--- src/simcity/gui/market/MarketCustomerGui.java
+++ src/simcity/gui/market/MarketCustomerGui.java
@@ -2,7 +2,10 @@ package simcity.gui.market;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Point;
+
+import javax.swing.ImageIcon;
 
 import simcity.Location;
 import simcity.buildings.market.MarketCustomerRole;
@@ -12,17 +15,24 @@ import simcity.interfaces.market.MarketCustomer;
 public class MarketCustomerGui extends Gui {
 	
 	private final int CASHIER_X = 200;
-	private final int CASHIER_Y = 200;
+	private final int CASHIER_Y = 300;
+	
+
+	 ImageIcon ii = new ImageIcon("res/Person/PersonDown.png");
+	    Image img = ii.getImage();
+	    Image personimage = img.getScaledInstance(30, 30,  java.awt.Image.SCALE_SMOOTH); 
 	
 	
 	public MarketCustomerGui(MarketCustomerRole m) {
 		role = m;
-	} 
+	}
 	
 	@Override
 	public void draw(Graphics2D g) {
-		g.setColor(Color.CYAN);
-		g.fillRect(x, y, SIZE, SIZE);
+		//g.setColor(Color.CYAN);
+		//g.fillRect(x, y, SIZE, SIZE);
+		g.drawImage(personimage, x,y, null); //COMMENT this out to get old Gui Back- Josh
+
 	}
 	
 	public void DoGoToCashier() {

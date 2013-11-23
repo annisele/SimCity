5bf335c4cb412177bdae36647bacc6c6bb2d891d
levonne key <levonnekey@levonnes-MacBook-Pro.local>
2013-11-23 13:40:02 -0800

    update person agent

--- src/simcity/PersonAgent.java
+++ src/simcity/PersonAgent.java
@@ -11,6 +11,7 @@ import simcity.gui.bank.BankCustomerGui;
 import simcity.gui.restaurantone.RestaurantOneCustomerGui;
 import simcity.gui.transportation.PedestrianGui;
 import simcity.interfaces.Person;
+import simcity.interfaces.bank.BankCustomer;
 import simcity.interfaces.house.HouseInhabitant;
 import simcity.interfaces.market.MarketCustomer;
 import simcity.interfaces.transportation.Pedestrian;
@@ -169,6 +170,36 @@ public class PersonAgent extends Agent implements Person {
 			
 			e = new Event(buildingName, eventR, 120, -1, true, steps, t);
 			//Do("GoToMarket is scheduled, which has "+steps.size()+" steps");
+			insertEvent(e);
+			stateChanged();
+		}
+		else if (t == EventType.DepositMoney) {
+			List<String> banks = Directory.getBanks();
+			int index = rand.nextInt(banks.size());
+			String buildingName = banks.get(index);
+			List<Step> steps = new ArrayList<Step>();
+			steps.add(new Step("exitBuilding", this));
+			steps.add(new Step("goTo", this));
+			steps.add(new Step("enterBuilding", this));
+			Role eventR = null;
+			for(Role r : myRoles) {
+				if(r instanceof BankCustomer) {
+					eventR = r;
+				}
+			}
+			HouseInhabitantRole house = null;
+			for(Role r : myRoles) {
+				if(r instanceof HouseInhabitantRole) {
+					house = (HouseInhabitantRole) r;
+				}
+			}
+
+			//hack
+			BankCustomerRole bc = new BankCustomerRole(this);
+			((BankCustomer)eventR).msgMoneyIsDeposited(bc, 123456, 1500, 1000);
+			
+			e = new Event(buildingName, eventR, 120, -1, true, steps, t);
+			
 			insertEvent(e);
 			stateChanged();
 		}

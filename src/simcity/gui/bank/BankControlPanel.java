b78a42277e75e54fa23ebb9befb7be44880037cd
levonne key <levonnekey@guest-wireless-upc-nat-206-117-88-005.usc.edu>
2013-11-22 18:30:22 -0800

    updated bank control panel

--- src/simcity/gui/bank/BankControlPanel.java
+++ src/simcity/gui/bank/BankControlPanel.java
@@ -10,6 +10,9 @@ import java.util.*;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import simcity.buildings.bank.BankCustomerRole;
+import simcity.buildings.bank.BankHostRole;
+import simcity.buildings.bank.BankTellerRole;
 import simcity.gui.AnimationPanel;
 import simcity.gui.Gui;
 import simcity.gui.SimCityGui;
@@ -22,9 +25,14 @@ import simcity.gui.SimCityGui;
 public class BankControlPanel extends JPanel {
 	
 	private JLabel typeLabel = new JLabel("Type: Bank");
-	
+	int windows = 3;
 	public BankControlPanel() {
 		add(typeLabel);
 	}
+	private Vector<BankCustomerRole> bankCustomers = new Vector<BankCustomerRole>();
+	private ArrayList<BankTellerRole> bankTeller = new ArrayList<BankTellerRole>();
+	private BankHostRole bankHost = new BankHostRole("Bank Host", windows);
+	
+	
 	
 }

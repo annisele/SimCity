5f0584c68b138cbb04e3e8946c80533b81f648fd
levonne key <levonnekey@levonnes-MacBook-Pro.local>
2013-11-22 23:55:04 -0800

    updated bank gui

--- src/simcity/gui/bank/BankCustomerGui.java
+++ src/simcity/gui/bank/BankCustomerGui.java
@@ -3,14 +3,12 @@ package simcity.gui.bank;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Point;
-
 import simcity.Location;
 import simcity.buildings.bank.BankCustomerRole;
 import simcity.gui.Gui;
 
 public class BankCustomerGui extends Gui{
 
-	private BankCustomerRole role = null;
 	private int x = 0;
 	private int y = 0;
 	private int xDest = 0;

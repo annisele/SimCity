0d6f8f91873fa6e98a0974cc4589b3c205acfddd
levonne key <levonnekey@guest-wireless-upc-nat-206-117-88-005.usc.edu>
2013-11-24 20:38:56 -0800

    change bank guis

--- src/simcity/gui/bank/BankCustomerGui.java
+++ src/simcity/gui/bank/BankCustomerGui.java
@@ -30,7 +30,7 @@ public class BankCustomerGui extends Gui{
 	}
 	
 	public void draw(Graphics2D g) {
-		g.drawImage(img, x, y, null); 
+		g.drawImage(img, getX(), getY(), null); 
 	}
 	public void DoGoToHost() {
 		DoGoToLocation(HOST_X, HOST_Y);

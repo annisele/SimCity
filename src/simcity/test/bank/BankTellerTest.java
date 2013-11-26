170c8999549ca7098d2920a6c435216472253910
levonne key <levonnekey@guest-wireless-upc-nat-206-117-88-005.usc.edu>
2013-11-25 17:01:02 -0800

    updated tests for bank

---
+++ src/simcity/test/bank/BankTellerTest.java
@@ -0,0 +1,24 @@
+package simcity.test.bank;
+
+import simcity.buildings.bank.BankTellerRole;
+import junit.framework.*;
+/**
+ * 
+ * This class is a JUnit test class to unit test the BankHostRole's basic interaction
+ * with waiters, customers, and the host.
+ *
+ * @author Levonne Key
+ */
+public class BankTellerTest extends TestCase {
+	BankTellerRole bankTeller;
+	//MockBankHost bankHost;
+	//MockBankCustomer bankCustomer;
+	
+
+	/**
+	 * This method is run before each test. You can use it to instantiate the class variables
+	 * for your role and mocks, etc.
+	 */
+	
+	
+}

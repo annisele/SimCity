0fb2eaf4e00ae7a86b6da011c89b620a58dba54c
clay-to-n <cbrand@usc.edu>
2013-11-25 20:59:01 -0800

    Working on a One House One Market scenario

--- src/simcity/Config.java
+++ src/simcity/Config.java
@@ -149,7 +149,7 @@ public class Config {
 		systems.setBackgroundTwo();
 		
 		systems.addMarket("Market", 60, 140);
-		systems.addHouse("House", 249, 140);
+		systems.addHouse("HouseOne", 249, 140);
 		
 		systems.addMarketCashierHack("Mary", "Market");
 		systems.addMarketTruck("Market");
@@ -159,6 +159,9 @@ public class Config {
 				systems.addMarketWorkerHack("Bob", "Market");
 			}
 		}, 2000);
+		
+		systems.addPerson("Tony");
+		systems.setHome("Tony", "HouseOne");
 		
 		// These are the right-most houses
 		//systems.addHouse("HouseTwo", 402, 27);

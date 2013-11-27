10d500d8d6eaa0bbeaef73ddafacd4c13ea6bec8
mich326 <mich326@usc.edu>
2013-11-26 16:13:34 -0800

    Preconditions of BusPassengerTest updated

--- src/simcity/test/transportation/BusPassengerTest.java
+++ src/simcity/test/transportation/BusPassengerTest.java
@@ -1,14 +1,16 @@
 package simcity.test.transportation;
 
-import junit.framework.TestCase;
+import junit.framework.*;
 import simcity.PersonAgent;
 import simcity.buildings.transportation.BusPassengerRole;
+import simcity.buildings.transportation.BusPassengerRole.PassengerState;
 import simcity.test.transportation.mock.MockBus;;
 
 public class BusPassengerTest extends TestCase {
 	BusPassengerRole buspassenger;
 	MockBus bus;
 	PersonAgent person = new PersonAgent("Traveler");
+
 	
 	public void setUp() throws Exception {
 		super.setUp();
@@ -17,10 +19,20 @@ public class BusPassengerTest extends TestCase {
 		
 	}
 	
-	public void TestNormBusPassenger() {
+	public void testnorm() {
 		//Checking PreConditions
-		//assertEquals("Passenger's State should be OffBus at the beginning of the Scenario")
+		assertEquals("Passenger's State should be OffBus at the beginning of the Scenario", buspassenger.state, PassengerState.offBus );
+		assertEquals("Passenger's Bus shouldn't be set yet, so it should return null. It doesn't", buspassenger.bus, null);
+		assertEquals("Passenger's xCoordinate should be initialized at zero", buspassenger.xLoc, 0);
+		assertEquals("Passenger's yCoordinate should be initialized at zero", buspassenger.yLoc, 0);
+		assertEquals("Passenger's startLocation should not be set", buspassenger.startingLocation,0);
+		assertEquals("Passenger's destination should not be set", buspassenger.destination,0);
 		
+		
+		
+
+		
+
 		
 	}
 	

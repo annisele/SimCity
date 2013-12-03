0fb01ec7acd7aa6681995385df9dae6b2bedd599
mich326 <mich326@usc.edu>
2013-12-03 15:09:23 -0800

    Restaurant One Gui Files ready to be added, error free

--- src/simcity/gui/restaurantone/FoodGui.java
+++
@@ -1,1 +0,0 @@
-package simcity.gui.restaurantone;
--- src/simcity/gui/restaurantone/RestaurantOneHostGui.java
+++ src/simcity/gui/restaurantone/RestaurantOneHostGui.java
@@ -1,35 +1,51 @@
 package simcity.gui.restaurantone;
 
-import java.awt.Graphics2D;
-import java.awt.Point;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+import simcity.buildings.restaurant.one.*;
 import simcity.gui.Gui;
 
-public class RestaurantOneHostGui implements Gui{
+public class RestaurantOneHostGui extends Gui {
 
-	@Override
-	public void updatePosition() {
-		// TODO Auto-generated method stub
-		
-	}
+    private RestaurantOneHostRole hostrole = null;
 
-	@Override
-	public void draw(Graphics2D g) {
-		// TODO Auto-generated method stub
-		
-	}
+    private int xPos = 200, yPos = 100;//default waiter position
+            
 
-	@Override
-	public boolean isPresent() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    public RestaurantOneHostGui(RestaurantOneHostRole hr) {
+        this.hostrole = hr;
+    }
+    
+    // retrieve table location information from the animation panel
+    // pass the table location information to the Customer GUI (the customer cannot access the info, but the GUI can)
+    // pass the table location information to the WAITER Gui
 
-	@Override
-	public boolean contains(Point point) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	
-	
+
+    public void draw(Graphics2D g) {
+            g.setColor(new Color(250,250, 250));
+    }
+
+    public boolean isPresent() {
+        return true;
+    }
+
+    public int getXPos() {
+        return xPos;
+    }
+
+    public int getYPos() {
+        return yPos;
+    }
+    
+
+        @Override
+        public void updatePosition() {
+                // TODO Auto-generated method stub
+                
+        }
 }

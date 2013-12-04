f4c6c03522fe49560c32fb12c5b011953b0dc4d0
Kevin Chan <kevinalc@usc.edu>
2013-12-03 20:28:24 -0800

    Edited restaurant four files to accommodate unit testing

--- src/simcity/interfaces/restaurant/four/RestaurantFourCustomer.java
+++ src/simcity/interfaces/restaurant/four/RestaurantFourCustomer.java
@@ -1,5 +1,29 @@
 package simcity.interfaces.restaurant.four;
 
-public interface RestaurantFourCustomer {
+import simcity.PersonAgent;
+import simcity.SimSystem;
+import simcity.buildings.restaurant.four.RestaurantFourCustomerRole.Event;
+import simcity.buildings.restaurant.four.RestaurantFourCustomerRole.State;
+import simcity.buildings.restaurant.four.RestaurantFourSystem;
+import simcity.interfaces.GuiPartner;
 
+public interface RestaurantFourCustomer extends GuiPartner {
+
+	// Accessors
+	public abstract void setPersonAgent(PersonAgent person);
+	public abstract RestaurantFourSystem getSystem();
+	public abstract void setSystem(RestaurantFourSystem restaurantFourSystem);
+	public abstract State getState();
+	public abstract void setState(State state);
+	public abstract Event getEvent();
+	public abstract void setEvent(Event event);
+	
+	// Messages
+	public abstract void msgGotHungry();
+	
+	// Utilities
+	public abstract void exitBuilding();
+	public abstract void enterBuilding(SimSystem s);
+	public abstract void atDestination();
+	
 }

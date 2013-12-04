4396d4f4410bcc0df67e53349df4a6490d6eb246
levonne key <levonnekey@guest-wireless-upc-nat-206-117-88-004.usc.edu>
2013-12-03 22:10:12 -0800

    update restaurant 3

--- src/simcity/buildings/restaurant/three/RestaurantThreeWaiterRole.java
+++ src/simcity/buildings/restaurant/three/RestaurantThreeWaiterRole.java
@@ -1,19 +1,34 @@
 package simcity.buildings.restaurant.three;
 
+import java.util.Timer;
 import java.util.concurrent.Semaphore;
 
+import simcity.PersonAgent;
+import simcity.Role;
+import simcity.SimSystem;
+import simcity.gui.bank.BankTellerGui;
+import simcity.gui.restaurantthree.RestaurantThreeWaiterGui;
+import simcity.gui.trace.AlertLog;
+import simcity.gui.trace.AlertTag;
 import simcity.interfaces.restaurant.three.RestaurantThreeCashier;
 import simcity.interfaces.restaurant.three.RestaurantThreeCook;
 import simcity.interfaces.restaurant.three.RestaurantThreeHost;
 import simcity.interfaces.restaurant.three.RestaurantThreeWaiter;
-public class RestaurantThreeWaiterRole implements RestaurantThreeWaiter{
-
-private Semaphore atDest = new Semaphore(0, true);
-	
+import simcity.test.mock.EventLog;
+public class RestaurantThreeWaiterRole extends Role implements RestaurantThreeWaiter{
+	private Timer timer = new Timer();
+	public  EventLog log = new EventLog();
+	private String name;
+	private Semaphore atDest = new Semaphore(0, true);
+	private RestaurantThreeSystem restaurantThreeSystem;
 	public void atDestination() {
 		atDest.release();
 	}
-
+	public RestaurantThreeWaiterRole(PersonAgent person, RestaurantThreeSystem rest) {
+		this.person = person;
+		this.gui = new RestaurantThreeWaiterGui(this);
+		this.restaurantThreeSystem = rest;
+	}
 	@Override
 	public void setHost(RestaurantThreeHost host) {
 		// TODO Auto-generated method stub
@@ -29,6 +44,36 @@ private Semaphore atDest = new Semaphore(0, true);
 	@Override
 	public void setCashier(RestaurantThreeCashier cashier) {
 		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean pickAndExecuteAnAction() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void exitBuilding() {
+		AlertLog.getInstance().logMessage(AlertTag.valueOf(restaurantThreeSystem.getName()), "Restaurant Three Waiter: " + person.getName(), "Leaving restaurant three");	
+		gui.DoExitBuilding();
+		try {
+			atDest.acquire();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		restaurantThreeSystem.exitBuilding(this);
+		person.roleFinished();
+		
+	}
+
+	@Override
+	public void enterBuilding(SimSystem s) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public class MyCustomer {
 		
 	}
 }
---
+++ src/simcity/gui/restaurantthree/RestaurantThreeWaiterGui.java
@@ -0,0 +1,45 @@
+package simcity.gui.restaurantthree;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Point;
+
+import javax.swing.ImageIcon;
+
+import simcity.Location;
+import simcity.buildings.bank.BankTellerRole;
+import simcity.buildings.restaurant.three.RestaurantThreeWaiterRole;
+import simcity.gui.Gui;
+import simcity.interfaces.bank.BankTeller;
+
+public class RestaurantThreeWaiterGui extends Gui {
+		
+	private final int CORRIDOR_X = 15;
+	private final int CORRIDOR_Y = 85;
+			;
+	private final int WINDOW1_X = 105;
+	private final int WINDOW1_Y = 103;
+	
+	private final int WINDOW2_X = 200;
+	private final int WINDOW2_Y = 103;
+		
+	private final int WINDOW3_X = 280;
+	private final int WINDOW3_Y = 103;
+
+	private final int HOST_X = 250;
+	private final int HOST_Y = 400;
+	
+	ImageIcon ii = new ImageIcon("res/person/tanpersondownbig.png");
+	Image img = ii.getImage();
+
+
+	public RestaurantThreeWaiterGui(RestaurantThreeWaiterRole b) {
+		role = b;
+	}
+	
+	public void draw(Graphics2D g) {
+		g.drawImage(img,getX(), getY(), null);
+	}
+
+}

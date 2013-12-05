bb769bc2c402e85c85ea95128eb8ed1b5bc050a9
levonne key <levonnekey@guest-wireless-upc-nat-206-117-88-019.usc.edu>
2013-12-04 22:42:23 -0800

    udpate restaurant 3

--- src/simcity/buildings/restaurant/three/RestaurantThreeWaiterRole.java
+++ src/simcity/buildings/restaurant/three/RestaurantThreeWaiterRole.java
@@ -1,5 +1,8 @@
 package simcity.buildings.restaurant.three;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Timer;
 import java.util.concurrent.Semaphore;
 
@@ -14,11 +17,19 @@ import simcity.interfaces.restaurant.three.RestaurantThreeCook;
 import simcity.interfaces.restaurant.three.RestaurantThreeHost;
 import simcity.interfaces.restaurant.three.RestaurantThreeWaiter;
 import simcity.test.mock.EventLog;
+
+/**
+ * Restaurant Three Waiter Role
+ * @author Levonne Key
+ *
+ */
 public class RestaurantThreeWaiterRole extends Role implements RestaurantThreeWaiter{
 	private Timer timer = new Timer();
 	private RestaurantThreeHost host;
 	private RestaurantThreeCook cook;
 	private RestaurantThreeCashier cashier;
+	public enum CustomerState {WAITING,READY_TO_ORDER,ORDERING,FOOD_SERVED,IS_DONE,NO_ACTION, READY_TO_PAY, EXPENSIVE_LEAVE, REORDER, CANNOT_AFFORD_TO_REORDER};
+	private List<MyCustomer> customers = Collections.synchronizedList(new ArrayList<MyCustomer>());
 	public  EventLog log = new EventLog();
 	private String name;
 	private Semaphore atDest = new Semaphore(0, true);
@@ -62,7 +73,7 @@ public class RestaurantThreeWaiterRole extends Role implements RestaurantThreeWa
 
 	@Override
 	public void enterBuilding(SimSystem s) {
-		// TODO Auto-generated method stub
+		restaurantThreeSystem = (RestaurantThreeSystem)s;
 		
 	}
 	

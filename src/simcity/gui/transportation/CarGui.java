12a4ea74c8d1c2d04938798a22d9bc40951e4d1c
mich326 <mich326@usc.edu>
2013-12-07 23:13:33 -0800

    Adding additions to CarAgent and CarGui

--- src/simcity/gui/transportation/CarGui.java
+++ src/simcity/gui/transportation/CarGui.java
@@ -1,5 +1,71 @@
 package simcity.gui.transportation;
 
-public class CarGui {
+import java.awt.Graphics2D;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+import simcity.Location;
+import simcity.gui.Gui;
+import simcity.buildings.transportation.*;
+
+public class CarGui extends Gui {
+	private CarAgent caragent;
+	private int x;
+	private int y;
+	private int xDest;
+	private int yDest;
+	
+	private boolean atDestNow = false;
+	
+	ImageIcon ii = new ImageIcon("res/transportation/bus.png");
+	Image img = ii.getImage();
+	Image busimage = img.getScaledInstance(25, 25,  java.awt.Image.SCALE_SMOOTH); 
+	
+	public CarGui(CarAgent cahh) {
+		this.caragent = cahh;
+		x= 20;
+		y= 108;
+		
+		
+	}
+	
+	public void updatePosition() {
+		if(xDest+1 > x) {
+			x++;
+		}
+		else {
+			x--;
+		}
+		if(yDest+1 > y) {
+			y++;
+		}
+		else {
+			y--;
+		}
+		if (((x == (xDest)) || (x == (xDest+1))) && ((y == (yDest)) || (y == (yDest+1)))) {
+			if (atDestNow == false) {
+				caragent.atDestination();
+				atDestNow = true;
+			}
+		} 
+	}
+    
+	@Override
+	public void draw(Graphics2D g) {
+		g.drawImage(busimage, x, y, null); //COMMENT this out to get old Gui Back- Josh
+	}
+	
+	public void DoGoTo(int x, int y) {
+		//atDestNow = false;
+		xDest = x;
+		yDest = y;
+		atDestNow = false; 
+	}
+
+	public void setCarAgent(CarAgent car) {
+		this.caragent = car;
+	}
+
 
 }

6d177fc4ddb207048891643f5f71ca02a0a9ebb8
Kevin Chan <kevinalc@usc.edu>
2013-12-08 16:03:18 -0800

    Added restaurant4 customer gui

--- src/simcity/SystemManager.java
+++ src/simcity/SystemManager.java
@@ -151,6 +151,11 @@ public class SystemManager {
 		if (name.equalsIgnoreCase("Gus Fring") || name.equalsIgnoreCase("Ted Benacke") || name .equalsIgnoreCase("jack")) {
 			person.goToRestaurantOneNow();
 		}
+		
+		if (name == "Hungry R4Customer") {
+			person.goToRestaurantFourNow();
+		}
+		
 		people.add(person);
 		person.startThread();
 	}

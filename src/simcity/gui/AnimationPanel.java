750c7901229c04b41e8309844c3efa5211ca9cd3
fitzgerlard <yiwen@usc.edu>
2013-12-10 23:20:00 -0800

    l

--- src/simcity/gui/AnimationPanel.java
+++ src/simcity/gui/AnimationPanel.java
@@ -26,7 +26,7 @@ public class AnimationPanel extends JPanel implements ActionListener{
 
 	SimCityGui simCityGui;
 	JPanel controlPanel;
-	//PersonPanel pPanel= new PersonPanel();
+	PersonPanel pPanel= new PersonPanel();
 
 	protected List<Gui> guis = Collections.synchronizedList(new ArrayList<Gui>());
 	protected List<BuildingGui> buildingGuis = Collections.synchronizedList(new ArrayList<BuildingGui>());
@@ -78,8 +78,9 @@ public class AnimationPanel extends JPanel implements ActionListener{
 						//we can either just print out the object class name
 						System.out.println("Clicked a "+g.getClass().getName());
 					//	pPanel.setInfo(g);
-						//simCityGui.setControlPanel(pPanel);
-
+						if (simCityGui != null) {
+						simCityGui.setControlPanel(pPanel);
+						}
 						if(g instanceof MarketCashierGui) {
 						AlertLog.getInstance().logInfo(AlertTag.WORLD, "Mouse", "You clicked a " + g.getClass().getSimpleName() + "!");
 						}
